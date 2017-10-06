fdcece7c83e01a1f5cd7d30e7b153ffab0bc402e
Larry Cromwell <lcromwel@ovpn-126-15.rdu2.redhat.com>
2017-10-06 14:31:54 -0400

    Thought this was already pushed...

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ import socket
 
 app = Flask(__name__)
 
-# "img src = 'static/ArnoldSchwartzenegger.jpeg' ><br>" \
+# "<img src = 'static/oc.png' ><br>" \
+# "<img src = 'static/ArnoldSchwartzenegger.jpeg' > <br>" \
 
 @app.route("/")
 def hello():
         
     html = "<h2>Hello {name}!</h2>" \
-        "<img src = 'static/oc.png' ><br>" \
+        "<img src = 'static/ArnoldSchwartzenegger.jpeg' > <br>" \
         "<h3>Hostname is dynamically named by container runtime platform:</h3>" \
         "<b>Hostname is </b> {hostname}<br/>" \
     
